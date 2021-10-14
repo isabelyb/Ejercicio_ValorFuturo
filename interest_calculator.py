7796e9093b7a2c022eaafe8d14c0c53ebd8eff7a
isabelyb <isabelcristinayepesb@gmail.com>
2021-10-14 14:53:36 -0500

    finishing bonus

--- interest_calculator.py
+++ interest_calculator.py
@@ -8,6 +8,7 @@ def main():
     2 -> Present Value
     3 -> Interest period
     4 -> Total period
+    5 -> Nominal interest by the period
 
     Option: """    
 
@@ -20,7 +21,9 @@ def main():
     elif option == 3:
         print(f'The periodic interest calculated is {p_interest()} %')
     elif option == 4:
-        print(f'The total period calculated is {t_period()} months')
+        print(f'The total period calculated is {t_period()} periods')
+    elif option == 5:
+        print(f'The nominal interest by period calculated is {n_interest()} %')
     else:
         print("Select a correct option")
         main()
@@ -29,7 +32,7 @@ def main():
 def f_value():
     p_val = float(input('How much is the present value? '))
     p_int = float(input('What is the period interest in percentage? '))
-    t_per = float(input('What is the total period in months? '))
+    t_per = float(input('What is the total periods? '))
     f_val = round((p_val * (1 + p_int/100)**t_per), 1)
     return f_val
 
@@ -37,17 +40,17 @@ def f_value():
 def p_value():
     f_val = float(input('How much is the future value? '))
     p_int = float(input('What is the period interest in percentage? '))
-    t_per = float(input('What is the total period in months? '))
+    t_per = float(input('What is the total periods? '))
     p_val = round((f_val / (1 + p_int/100)**t_per), 1)
     return p_val
 
 
 def p_interest():
     p_val = float(input('How much is the present value? '))
-    t_per = float(input('What is the total period in months? '))
+    t_per = float(input('What is the total periods? '))
     f_val = float(input('How much is the future value? '))
-    p_int = round(((f_val/p_val)**(1/t_per) - 1),2)
-    return p_int*100
+    p_int = (f_val/p_val)**(1/t_per) - 1
+    return round((p_int*100),2)
 
 
 def t_period():
@@ -56,6 +59,13 @@ def t_period():
     p_int = float(input('What is the period interest in percentage? '))
     t_per = round((np.log(f_val/p_val) / np.log(1+(p_int/100))), 1)
     return t_per
+
+
+def n_interest():
+    p_int = float(input('What is the period interest in percentage? '))
+    t_per = float(input('What is the total periods? '))
+    n_int = ((((p_int/100)+1)**(1/t_per))-1)*t_per
+    return round((n_int*100),5)
 
 
 if __name__ == '__main__':
